e3f224a4ce467c17f853e98d80604e73ef676831
Rachel <rachjanehall@gmail.com>
2018-04-20 17:27:41 +1000

    First push and setup complete

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+postCSS = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins'),
+hexrgba = require('postcss-hexrgba');
+
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postCSS([cssImport, autoprefixer, cssvars, nested, mixins, hexrgba]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
+});
